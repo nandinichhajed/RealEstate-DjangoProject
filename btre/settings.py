45e31c49c6e7e03a19c74df3e8982feb243e8919
nandinichhajed <nandinichhajed08@gmail.com>
2021-06-22 20:22:50 +0530

    Main

--- btre/settings.py
+++ btre/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
-    'listings.apps.ListingsConfig',
+    'listings.apps.ListingConfig',
     'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
