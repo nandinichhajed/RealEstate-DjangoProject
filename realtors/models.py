1f6e274aba939b132dab27f9c02dacd24989bc11
nandinichhajed <nandinichhajed08@gmail.com>
2021-06-23 23:56:31 +0530

    realtors

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Listing(models.Model):
+    name = models.CharField(max_Length = 200)
+    photo = models.ImageField(upload_to = 'photos/%y/%m/%d/')
+    description = models.TextField(blank = True)
+    phone = models.CharField(max_Length = 20)
+    email = models.CharField(max_Length = 20)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(defaulat = datetime.now, blank=True)
+    def __str__(self):
+        return self.name
