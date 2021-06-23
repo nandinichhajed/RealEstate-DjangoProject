0a660942a8d75ce1e5c0570a73fbfc59e033e4f9
nandinichhajed <nandinichhajed08@gmail.com>
2021-06-24 01:11:09 +0530

    Listings

--- listings/models.py
+++ listings/models.py
@@ -1,29 +1,31 @@
+from typing import Optional
 from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
 
 class Listing(models.Model):
-    relator = models.ForeignKey(relator, on_delete=models.DO_NOTHING)
-    title = models.CharField(max_Length = 200)
-    address = models.CharField(max_Length = 200)
-    city= models.CharField(max_Length = 200)
-    state = models.CharField(max_Length = 200)
-    zipcode = models.CharField(max_Length = 200)
-    description = models.TextField(blank = True)
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+    description = models.TextField(blank=True)
     price = models.IntegerField()
-    bedrooms = models.DecimalField()
-    bathrooms = models.IntegerField(max_digits=2, decimal_places=1)
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    photo_main = models.ImageField(upload_to = 'photos/%y/%m/%d/')
-    photo_1 = models.ImageField(upload_to = 'photos/%y/%m/%d/', blank = True)
-    photo_2 = models.ImageField(upload_to = 'photos/%y/%m/%d/', blank = True)
-    photo_3 = models.ImageField(upload_to = 'photos/%y/%m/%d/', blank = True)
-    photo_4 = models.ImageField(upload_to = 'photos/%y/%m/%d/', blank = True)
-    photo_5 = models.ImageField(upload_to = 'photos/%y/%m/%d/', blank = True)
-    photo_6 = models.ImageField(upload_to = 'photos/%y/%m/%d/', blank = True)
+    photo_main = models.ImageField(upload_to= 'photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to= 'photos/%Y/%m/%d/', blank= True)
+    photo_2 = models.ImageField(upload_to= 'photos/%Y/%m/%d/', blank= True)
+    photo_3 = models.ImageField(upload_to= 'photos/%Y/%m/%d/', blank= True)
+    photo_4 = models.ImageField(upload_to= 'photos/%Y/%m/%d/', blank= True)
+    photo_5 = models.ImageField(upload_to= 'photos/%Y/%m/%d/', blank= True)
+    photo_6 = models.ImageField(upload_to= 'photos/%Y/%m/%d/', blank= True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(defaulat = datetime.now, blank=True)
+    list_date= models.DateTimeField(default=datetime.now, blank=True)
+
     def __str__(self):
-        return self.title
+        return self.title 
