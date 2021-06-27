4e31f131311abe2f5532375c51fe7dda03c0d250
nandinichhajed <nandinichhajed08@gmail.com>
2021-06-27 14:36:42 +0530

    Listings

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listing
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)
+    
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
 
-def listing(request):
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
 def search(request):
     return render(request, 'listings/search.html')
 
-# ./venv/Scripts/activate
-#  python manage.py runserver
