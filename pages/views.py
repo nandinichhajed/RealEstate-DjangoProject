84c9eb7d0d25ea4fdba66d591297c09cce37cb89
nandinichhajed <nandinichhajed08@gmail.com>
2021-06-28 14:52:50 +0530

    Pages

--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
 
     return render(request, 'pages/index.html', context)
