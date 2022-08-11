d5bb02a8479e9f64c5e093397a57830d06213f7f
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-11 21:40:31 +0530

    adding localhost

--- btre/settings.py
+++ btre/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["application-real-estate.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["application-real-estate.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
